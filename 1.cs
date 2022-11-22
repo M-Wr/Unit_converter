1f53e519cd2633f9ccde3f03453eb53da70f8970
Max Wright <max.wright@experian.com>
2022-11-22 11:43:59 +0000

    First Commit

---
+++ 1.cs
@@ -0,0 +1,36 @@
+namespace game {
+    using System;
+
+namespace Review
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      void input = Console.WriteLine();
+      var kilo = Int32.Parse(input);
+      var stone = kilo / 6.35;  
+      Console.WriteLine(stone);
+      var stoneDec = stone % 1;
+      
+      Console.WriteLine(stoneDec);
+
+      var pound = stoneDec * 14;
+      stone = Math.Truncate(stone);
+      pound = Math.Truncate(pound);
+
+      Console.WriteLine(pound);
+
+      var endp = $" {kilo} Kg, is equivilent to: {stone}st and {pound}lbs";
+      Console.WriteLine(endp);
+
+    }
+  }
+}
+
+
+
+
+
+}
+
